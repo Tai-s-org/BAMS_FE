cc027d9007d75b95c7e3b2bce3657e168ec7b7eb
conghieu281 <conghieu8899.work@gmail.com>
2025-02-27 20:39:13 +0700

    package setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NotificationProvider } from "../context/AlertProvider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NotificationProvider position="top-right">{children}</NotificationProvider>
       </body>
     </html>
   );
