57659f201fd46eeb94b58b30323269ba2d8c8964
taing28 <taing280703@gmail.com>
2025-05-05 15:29:45 +0700

    feat: contact-tracker

---
+++ src/app/contact-tracker/layout.js
@@ -0,0 +1,5 @@
+import DashboardLayout from "@/components/layout/DashboardLayout";
+
+export default function ContactTrackerDashboard({ children }) {
+    return <DashboardLayout>{children}</DashboardLayout>
+}
